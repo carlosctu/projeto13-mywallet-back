06ffa81c00ab4e718d4e9bb88e016da665fee885
carlos_casanova <carlosctu94@gmail.com>
2022-09-10 17:42:18 -0300

    [FIX] Fixing signUpSchema to not accept empty values

--- src/schemas/authSchemas.js
+++ src/schemas/authSchemas.js
@@ -1,9 +1,9 @@
 import joi from "joi";
 
 const signUpSchema = joi.object({
-  name: joi.string().required(),
-  email: joi.string().required(),
-  password: joi.string().required(),
+  name: joi.string().min(1).required(),
+  email: joi.string().min(3).required(),
+  password: joi.string().min(6).required(),
 });
 const signInSchema = joi.object({
   email: joi.string().required(),
